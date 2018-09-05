68e63a848a78b05b4d41b370ef82c2113ccf6874
Arun George Zachariah <az2z7@mail.umkc.edu>
2018-09-05 09:27:14 -0500

    Changes done the trigger the experiment from the UI

--- configure/views.py
+++ configure/views.py
@@ -13,11 +13,11 @@ def enterConfig(request) :
 			l = form.cleaned_data['l']
 			k = form.cleaned_data['k']
 			families = form.cleaned_data['families']
-			
+
 			print("n :: ",n , ":: l ::",l ," :: k :: ", k ,":: families :: " ,families)
 
 			#Script to be executed : create-config.sh
-			subprocess.call(shlex.split('./ShellScripts/test.sh -n ' + str(n) + ' -l ' + str(l) + ' -k ' + str(k) + ' -f ' + families))
+			subprocess.call(shlex.split("ssh arung@ms1144.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/ && bash demoExp.higgs.sh -n " + str(n) + " -l " + str(l) + "'"))
 
 	form = ConfigForm()
-	return render(request, '../templates/configForm.html', {'form':form})
+	return render(request, '../templates/config.html', {'form':form})
