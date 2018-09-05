cb7108c892942b6fa0e3d1cf5afa0c6ce91e28c0
Arun George Zachariah <az2z7@mail.umkc.edu>
2018-09-05 10:47:05 -0500

    Adding a dropdown for listing the datasets

--- configure/forms.py
+++ configure/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
+EXP_CHOICES=[
+('higgs','HIGGS Dataset'),
+('syn', 'Synthetic Dataset'),
+('twtr', 'Twitter Dataset'),
+]
+
 class ConfigForm(forms.Form):
 	n = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 	l = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 	k = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-	families = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	dataset = forms.CharField(widget=forms.Select(choices=EXP_CHOICES, attrs={'class': 'form-control'}), label = "Dataset :")
--- configure/views.py
+++ configure/views.py
@@ -12,12 +12,15 @@ def enterConfig(request) :
 			n = form.cleaned_data['n']
 			l = form.cleaned_data['l']
 			k = form.cleaned_data['k']
-			families = form.cleaned_data['families']
+			dataset = form.cleaned_data['dataset']
 
-			print("n :: ",n , ":: l ::",l ," :: k :: ", k ,":: families :: " ,families)
-
-			#Script to be executed : create-config.sh
-			subprocess.call(shlex.split("ssh arung@ms1144.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/ && bash demoExp.higgs.sh -n " + str(n) + " -l " + str(l) + "'"))
+			if dataset == 'higgs' :
+				print("Executing Higgs")
+				subprocess.call(shlex.split("ssh arung@ms1144.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/ && bash demoExp.higgs.sh -n " + str(n) + " -l " + str(l) + "'"))
+			elif dataset == 'syn' :
+				print("Executing syn")
+			elif dataset == 'twtr' :
+				print("Executing twtr")
 
 	form = ConfigForm()
 	return render(request, '../templates/config.html', {'form':form})
