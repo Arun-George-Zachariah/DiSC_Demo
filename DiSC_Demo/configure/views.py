684506657f14f16f7000b78677afb12e498e904a
Arun George Zachariah <az2z7@mail.umkc.edu>
2018-09-29 18:20:57 -0500

    Changes done to Check if gossiping is in progress.

--- DiSC_Demo/configure/views.py
+++ DiSC_Demo/configure/views.py
@@ -10,35 +10,41 @@ def enterConfig(request) :
 
 	#Check for any existing gossip process running.
 	#To-Do : If so the user needs to be alterted that a gossip process is running.
+	try:
+		output = subprocess.check_output(shlex.split("ssh arung@ms1020.utah.cloudlab.us 'jps | grep jar'"))
+		if not (output is None):
+			return render(request, '../templates/error.html')
+	except:
+		print("No Existing Gossip Process Found")
 
-	if request.method == 'POST' :
-		form = ConfigForm(request.POST)
-		if form.is_valid() :
-			n = form.cleaned_data['n']
-			l = form.cleaned_data['l']
-			k = form.cleaned_data['k']
-			r = form.cleaned_data['r']
-			dataset = form.cleaned_data['dataset']
-			#Hardcoded family. Needs to be in a dropdown
-			family = "jet_4_eta,jet_2_b-tag,jet_2_phi"
+		if request.method == 'POST' :
+			form = ConfigForm(request.POST)
+			if form.is_valid() :
+				n = form.cleaned_data['n']
+				l = form.cleaned_data['l']
+				k = form.cleaned_data['k']
+				r = form.cleaned_data['r']
+				dataset = form.cleaned_data['dataset']
+				#Hardcoded family. Needs to be in a dropdown
+				family = "jet_4_eta,jet_2_b-tag,jet_2_phi"
 
-			if dataset == 'HIGGS_Dataset' :
-				logFile = "/users/arung/higgs.r" + str(r) + ".k" + str(k) + ".txt"
-				#Stopping an existing gossip process
-				subprocess.call(shlex.split("ssh arung@ms1020.utah.cloudlab.us 'bash stopHiggsGossip.sh " + str(n-1) + "'"))
-				#Initializing the Streaming process.
-				subprocess.call(shlex.split("ssh arung@ms1020.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/DemoExecScripts/higgs && bash startStreaming.sh " + str(n-1) + " " + family + " " + logFile + " /users/arung/higgsTrueCounts.txt'"))
-				#Starting the gossip process
-				_thread.start_new_thread(executeShell , ("ssh arung@ms1020.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/DemoExecScripts/higgs && bash runDemo.higgs.sh " + str(n) + " " + str(k) + " " + str(l) + " " + str(r) + " " + family +"'",))
-			elif dataset == 'Synthetic_Dataset' :
-				_thread.start_new_thread(executeShell , ("ssh arung@ms1040.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/ && bash runDemo.syn.sh " + str(n) + " " + str(k) + " " + str(l) + " " + str(r) + "'",))
-			elif dataset == 'Twitter_Dataset' :
-				_thread.start_new_thread(executeShell , ("ssh arung@ms1040.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/ && bash runDemo.twtr.sh " + str(n) + " " + str(k) + " " + str(l) + " " + str(r) + "'",))
+				if dataset == 'HIGGS_Dataset' :
+					logFile = "/users/arung/higgs.r" + str(r) + ".k" + str(k) + ".txt"
+					#Stopping an existing gossip process
+					subprocess.call(shlex.split("ssh arung@ms1020.utah.cloudlab.us 'bash stopHiggsGossip.sh " + str(n-1) + "'"))
+					#Initializing the Streaming process.
+					subprocess.call(shlex.split("ssh arung@ms1020.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/DemoExecScripts/higgs && bash startStreaming.sh " + str(n-1) + " " + family + " " + logFile + " /users/arung/higgsTrueCounts.txt'"))
+					#Starting the gossip process
+					_thread.start_new_thread(executeShell , ("ssh arung@ms1020.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/DemoExecScripts/higgs && bash runDemo.higgs.sh " + str(n) + " " + str(k) + " " + str(l) + " " + str(r) + " " + family +"'",))
+				elif dataset == 'Synthetic_Dataset' :
+					_thread.start_new_thread(executeShell , ("ssh arung@ms1040.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/ && bash runDemo.syn.sh " + str(n) + " " + str(k) + " " + str(l) + " " + str(r) + "'",))
+				elif dataset == 'Twitter_Dataset' :
+					_thread.start_new_thread(executeShell , ("ssh arung@ms1040.utah.cloudlab.us 'cd /users/arung/DiSC_SRC/scripts/general/ && bash runDemo.twtr.sh " + str(n) + " " + str(k) + " " + str(l) + " " + str(r) + "'",))
 
-			return render(request, '../templates/data.html', {'N':n,'L':l,'K':k,'R':r,'Dataset':dataset})
+				return render(request, '../templates/data.html', {'N':n,'L':l,'K':k,'R':r,'Dataset':dataset})
 
-	form = ConfigForm()
-	return render(request, '../templates/config.html', {'form':form})
+		form = ConfigForm()
+		return render(request, '../templates/config.html', {'form':form})
 
 def executeShell(command) :
 	print("Excecuting shell")
